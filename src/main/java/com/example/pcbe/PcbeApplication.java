9c602631faa2b7822c5553dced264f0984583924
OVinter <ovidiu.vinter99@gmail.com>
2021-10-13 18:00:10 +0300

    Start

---
+++ src/main/java/com/example/pcbe/PcbeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pcbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcbeApplication.class, args);
+	}
+
+}

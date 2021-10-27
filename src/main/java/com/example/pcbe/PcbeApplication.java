fe6b42adba0265e1547d9419e13c5ded1551ff8a
OVinter <ovidiu.vinter99@gmail.com>
2021-10-27 12:35:22 +0300

    Remove SpringBoot

--- src/main/java/com/example/pcbe/PcbeApplication.java
+++ src/main/java/com/example/pcbe/PcbeApplication.java
@@ -1,13 +1,10 @@
 package com.example.pcbe;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class PcbeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PcbeApplication.class, args);
+
+		System.out.println("addsasd");
 	}
 
 }

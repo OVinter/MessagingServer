cfd989b7a349b75e84a96443e3ed4b85407cfae1
Dragos2099 <dragos.tudosoiu@student.upt.ro>
2021-11-08 21:43:04 +0200

    Added login and register

--- src/main/java/com/example/pcbe/JarRunner.java
+++ src/main/java/com/example/pcbe/JarRunner.java
@@ -3,22 +3,120 @@ package com.example.pcbe;
 import apps.CoffeeProviderApp;
 import apps.CoffeeShopApp;
 import apps.CustomerApp;
+import entities.users.User;
+import entities.users.UserType;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class JarRunner {
 
-    // TODO: based on User's name that is try to login, make a main call to that app
-    // ex: if User Alex is a CUSTOMER type then call CustomerApp.main(args);
-    public static void startApp(String[] args) {
-        if(args.length == 0) {
+    public static void startMainByType(String type)
+    {
+        String[] args = new String[0];
+        switch (type)
+        {
+            case "CUSTOMER":
+                CustomerApp.main(args);
+                break;
+            case "COFFEE_ADMIN":
+                CoffeeShopApp.main(args);
+                break;
+            case "PROVIDER":
+                CoffeeProviderApp.main(args);
+                break;
+            default:
+                System.out.println("Invalid user");
+        }
+    }
+
+    public static String getUserTypeFromDatabase(String username)
+    {
+        DbContext dbContext = DbContext.getInstance();
+        String sql = "SELECT name, type " +
+                "FROM users";
+        try{
+            Statement stmt = dbContext.getDbConnection();
+            ResultSet rs    = stmt.executeQuery(sql);
+            while (rs.next()) {
+                if(username.equals(rs.getString("name")))
+                    return rs.getString("type");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "NO_TYPE";
+    }
+
+    private static UserType getUserTypeFromString(String type)
+    {
+        switch (type)
+        {
+            case "CUSTOMER":
+                return UserType.CUSTOMER;
+            case "SHOP":
+                return UserType.COFFEE_ADMIN;
+            case "PROVIDER":
+                return UserType.PROVIDER;
+            default:
+                System.out.println("Not a valid type");
+        }
+        return null;
+    }
+
+    public static void addUserToDb(String username, UserType userType)
+    {
+        DbContext dbContext = DbContext.getInstance();
+        User newUser = new User(username, userType);
+
+        try(Statement stmt = dbContext.getDbConnection())
+        {
+            String sql = String.format("INSERT INTO USERS VALUES (%d,\"%s\",\"%s\")",newUser.getId(),newUser.getName(),newUser.getUserType().toString());
+            stmt.executeUpdate(sql);
+            System.out.println("User registered");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void startApp(String[] args)
+    {
+        if(args.length == 0)
+        {
+            String username, type;
+            Scanner sc = new Scanner(System.in);
+            int choiceentry;
+            do {
+                System.out.println("\n-----MENU-----\n\n \"1\" Login \n \"2\" Register \n \"3\" Exit");
+                choiceentry = sc.nextInt();
+
+                switch (choiceentry)
+                {
+                    case 1:
+                        System.out.println("Username:");
+                        username = sc.next();
+                        startMainByType(getUserTypeFromDatabase(username));
+                        break;
+                    case 2:
+                        System.out.println("Name : ");
+                        username = sc.next();
+                        System.out.println("Type [CUSTOMER / SHOP / PROVIDER]: ");
+                        type = sc.next();
+                        addUserToDb(username, getUserTypeFromString(type));
+                        break;
+                    case 3:
+                        System.out.println("Exit");
+                        break;
+                    default:
+                        System.out.println("Choice must be a value between 1 and 3.");
+                }
+            } while (choiceentry != 3);
+        }
+        else
+        {
             PcbeApplication.main(args);
-        } else if("Customer".equals(args[0])) {
-            CustomerApp.main(args);
-        } else if("Shop".equals(args[0])) {
-            CoffeeShopApp.main(args);
-        } else if("Provider".equals(args[0])) {
-            CoffeeProviderApp.main(args);
-        } else {
-            System.out.println("Bad argument!\nTry again");
         }
     }
 

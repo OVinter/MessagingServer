d608c8fcf9599c1949277cb62b16697a01e0af53
OVinter <ovidiu.vinter99@gmail.com>
2021-11-10 23:56:30 +0200

    Kafka props to utility class

--- src/main/java/apps/CoffeeProviderApp.java
+++ src/main/java/apps/CoffeeProviderApp.java
@@ -30,20 +30,7 @@ public class CoffeeProviderApp{
     private static boolean stillRunning = true;
     private static int cnt = 0;
     private static AtomicInteger messageCounter;
-    public static Properties getKafkaProps(boolean isConsumer) {
-        Properties props = new Properties();
-        if(isConsumer) {
-            props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-            props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-            props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-            props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-consumer-group");
-        } else {
-            props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-            props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-            props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        }
-        return  props;
-    }
+
     public static void receiveMessage() {
         // TODO
         System.out.println(getKafkaProps(true));

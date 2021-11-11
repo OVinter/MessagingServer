5fc2a24775ded2be997b5d24f1a7fa3d25538bc4
soniadeca28 <sonia.deca@student.upt.ro>
2021-11-11 22:51:51 +0200

    Updated private message types

--- src/main/java/entities/messages/PrivateMessageType.java
+++ src/main/java/entities/messages/PrivateMessageType.java
@@ -1,9 +1,15 @@
 package entities.messages;
 
 public enum PrivateMessageType {
+    RUN_OUT_OF_COFFEE, // the message that the coffee shop sends to the provider
     PROVIDER_RESPONSE, // return the number of the coffees(more or less the needed stuff to make a coffee)
-    MENU_MESSAGE, // the coffee shop sends the menu to the customer
-    COFFEE_MESSAGE, // the customer send the message to the coffee shop (the customer wants a coffee)
-    RESPONSE_TO_CUSTOMER, // the coffee shop send to customer if the order was completed
-    RUN_OUT_OF_COFFEE, // the message that the coffee sends to the provider
+    ASK_MENU, // the customer asks the coffee shop for the menu
+    MENU_MESSAGE, // the coffee shop sends the number of coffees available
+    COFFEE_MESSAGE, // the customer send the message to the coffee shop (the customer wants N coffees)
+    NOT_ENOUGH_COFFEES, //the coffee shop notifies the customer there are not enough coffees as he'd like to order
+    BILL_CUSTOMER, // the coffee shop sends the bill to the customer
+    PAY_SHOP, //the customer pays the shop
+    GIVE_CHANGE, //if the customer pays too much money, the coffee shop gives them change
+
+
 }

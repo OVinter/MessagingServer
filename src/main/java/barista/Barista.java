454a21ba95bb6714ce582f9fcffe69510ae943a3
iuliadrgn <iuliadragan02@yahoo.com>
2021-12-09 19:20:07 +0200

    added Barista making coffee

--- src/main/java/barista/Barista.java
+++ src/main/java/barista/Barista.java
@@ -1,66 +1,16 @@
 package barista;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+public class Barista{
 
-import java.util.Random;
+    public Barista(barista.Semaphor.BaristaSemaphor semBarista, String name) {
+        this.name = name;
+        this.semBarista = semBarista;
+        new Thread(barista.Semaphor.BaristaSemaphor::addingCoffeesThread, name+"-MakingCoffee").start();
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Barista {
-
-    private String baristaName;
-    private static int[] coffeesList = new int[10];
-    private static boolean stillRunning = true;
-
-    public void makeCoffee() {
-        new Thread(() -> {
-            while(stillRunning) {
-                try {
-                    Thread.sleep(generateNumberForSleep());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                addCoffeeInList();
-            }
-        }).start();
     }
 
-    public void sellCoffee() {
-        Thread sellCoffee = new Thread(() -> {
-            while(stillRunning) {
 
-            }
-        });
-    }
+    barista.Semaphor.BaristaSemaphor semBarista;
+    String name;
 
-    public static void stopThreads() {
-        stillRunning = false;
-    }
-
-    public static int[] getCoffeesList() {
-        return coffeesList;
-    }
-
-    private long generateNumberForSleep() {
-        Random random = new Random();
-        double randomValue = 2 * random.nextDouble();
-        return (long)(randomValue * 1000);
-    }
-
-    private void addCoffeeInList() {
-        for (int i = 0; i < coffeesList.length; i++) {
-            if(coffeesList[i] == 0) {
-                System.out.println("i " + i);
-                coffeesList[i] = 1;
-                break;
-            }
-        }
-        for (int i = 0; i < coffeesList.length; i++) {
-            System.out.print(coffeesList[i] + " ");
-        }
-        System.out.println();
-    }
 }
